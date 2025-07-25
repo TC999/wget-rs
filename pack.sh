d17d66048eb49cf0f0442617a3b55f664e9dd440
陈生杂物房 <88823709+TC999@users.noreply.github.com>
2025-07-25 13:21:33 +0800

    修复路径

--- pack.sh
+++ pack.sh
@@ -1,11 +1,18 @@
+#!/bin/bash
+
+# 进入 bin 目录
+cd bin
+
+# 遍历所有子目录
 for dir in */; do
-  # 判断每个目录下是否有wget-rs*文件
-  if [ -f "${dir}wget-rs*" ]; then
-    # 去掉最后的斜杠得到目录名
-    foldername=${dir%/}
-    # 打包为zip格式
-    #zip "${foldername}.zip" "${dir}wget-rs*"
-    # 如果要tar.gz格式
-    tar -czvf "../${foldername}.tar.gz" -C "${dir}" wget-rs*
+  # 去掉最后的斜杠，获取目录名
+  dirname=${dir%/}
+  # 查找该目录下以 wget-rs 开头的文件（包含可执行文件）
+  filepath="$dir"wget-rs*
+  # 判断文件是否存在
+  if ls $filepath 1> /dev/null 2>&1; then
+    # 在 bin 上级目录下打包，压缩文件名为父级目录名
+    tar czf "../${dirname}.tar.gz" $filepath
+    echo "已打包: ${dirname}.tar.gz"
   fi
 done
